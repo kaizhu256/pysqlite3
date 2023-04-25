d9174f750bf264f5c619f34d2bbaf1f5e402af05
kai zhu <kaizhu256@gmail.com>
2023-04-24 20:48:13 -0500

    sqlite3_all

---
+++ sqlite3.c
@@ -0,0 +1,3 @@
+#undef SQLITE3_C2
+#include "sqlite3_all.c"
+#undef SQLITE3_C2
---
+++ sqlite3.h
@@ -0,0 +1,7 @@
+#ifndef SQLITE3_H_INCLUDED
+    #define SQLITE3_H_INCLUDED
+    #define SQLITE3_H2
+    #undef SQLITE3_C2
+    #undef SQLITE3_EXT_H2
+    #include "sqlite3_all.c"
+#endif
