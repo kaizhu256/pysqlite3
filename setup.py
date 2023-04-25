66b3b1b2e6d48b64c529b06592fd31dcc1ace51f
kai zhu <kaizhu256@gmail.com>
2023-04-25 03:48:42 -0500

    tweak

--- setup.py
+++ setup.py
@@ -16,48 +16,48 @@ import setuptools
 if sys.platform == "darwin":
     os.environ["CFLAGS"] = "-Qunused-arguments"
 
-class AmalgationLibSqliteBuilder(distutils.command.build_ext.build_ext):
-    description = "Builds a C extension using a sqlite3 amalgamation"
-    def build_extension(self, ext):
-        ext.define_macros = [
-            ("MODULE_NAME", '"pysqlite3.dbapi2"'),
-            # Feature-ful library.
-            ("SQLITE_ALLOW_COVERING_INDEX_SCAN", "1"),
-            ("SQLITE_ENABLE_FTS3", "1"),
-            ("SQLITE_ENABLE_FTS3_PARENTHESIS", "1"),
-            ("SQLITE_ENABLE_FTS4", "1"),
-            ("SQLITE_ENABLE_FTS5", "1"),
-            ("SQLITE_ENABLE_JSON1", "1"),
-            ("SQLITE_ENABLE_LOAD_EXTENSION", "1"),
-            ("SQLITE_ENABLE_MATH_FUNCTIONS", "1"),
-            ("SQLITE_ENABLE_RTREE", "1"),
-            ("SQLITE_ENABLE_STAT4", "1"),
-            ("SQLITE_ENABLE_UPDATE_DELETE_LIMIT", "1"),
-            ("SQLITE_SOUNDEX", "1"),
-            ("SQLITE_USE_URI", "1"),
-            # Always use memory for temp store.
-            ("SQLITE_TEMP_STORE", "3"),
-            # Increase the maximum number of "host parameters" which SQLite will accept
-            ("SQLITE_MAX_VARIABLE_NUMBER", "250000"),
-            # Increase maximum allowed memory-map size to 1TB
-            ("SQLITE_MAX_MMAP_SIZE", "1099511627776"),
-        ]
-        if sys.platform != "win32":
-            # Include math library, required for fts5.
-            ext.extra_link_args.append("-lm")
+ext = {
+    "extra_link_args": [],
+}
+if sys.platform != "win32":
+    # Include math library, required for fts5.
+    ext["extra_link_args"] += ["-lm"]
 
-        # ext.libraries.append('sqlite3')
-        distutils.command.build_ext.build_ext.build_extension(self, ext)
-
+# ext.libraries.append('sqlite3')
 
 if __name__ == "__main__":
     setuptools.setup(**{
         "cmdclass": {
-            "build_static": AmalgationLibSqliteBuilder,
+            "build_static": distutils.command.build_ext.build_ext,
         },
         "description": "DB-API 2.0 interface for Sqlite 3.x",
         "ext_modules": [
             setuptools.Extension(
+                define_macros=[
+                    ("MODULE_NAME", '"pysqlite3.dbapi2"'),
+                    # Feature-ful library.
+                    ("SQLITE_ALLOW_COVERING_INDEX_SCAN", "1"),
+                    ("SQLITE_ENABLE_FTS3", "1"),
+                    ("SQLITE_ENABLE_FTS3_PARENTHESIS", "1"),
+                    ("SQLITE_ENABLE_FTS4", "1"),
+                    ("SQLITE_ENABLE_FTS5", "1"),
+                    ("SQLITE_ENABLE_JSON1", "1"),
+                    ("SQLITE_ENABLE_LOAD_EXTENSION", "1"),
+                    ("SQLITE_ENABLE_MATH_FUNCTIONS", "1"),
+                    ("SQLITE_ENABLE_RTREE", "1"),
+                    ("SQLITE_ENABLE_STAT4", "1"),
+                    ("SQLITE_ENABLE_UPDATE_DELETE_LIMIT", "1"),
+                    ("SQLITE_SOUNDEX", "1"),
+                    ("SQLITE_USE_URI", "1"),
+                    # Always use memory for temp store.
+                    ("SQLITE_TEMP_STORE", "3"),
+                    # Increase the maximum number of "host parameters"
+                    # which SQLite will accept
+                    ("SQLITE_MAX_VARIABLE_NUMBER", "250000"),
+                    # Increase maximum allowed memory-map size to 1TB
+                    ("SQLITE_MAX_MMAP_SIZE", "1099511627776"),
+                ],
+                extra_link_args=ext["extra_link_args"],
                 name="pysqlite3._sqlite3",
                 sources=[
                     "pysqlite3/module.c",
